87c13d511928f8d265cc12bffc474d24f362f6bc
Ludovico Caldara - Oracle <80839073+lcaldara-oracle@users.noreply.github.com>
2021-04-09 16:32:04 +0200

    Getting ready for OCI Resource Manager

--- provider.tf
+++ provider.tf
@@ -46,8 +46,9 @@ variable "private_key_path" {
 # in override.tf.
 
 # ----------------------------------
-variable "opc_private_key_path" { 
-  description = "Path to your private SSH key for the SSH connections as opc, eg: \"~/.ssh/id_rsa\""
+variable "ssh_private_key" { 
+  description = "Private SSH key string corresponding to the ssh_public_key. e.g. \"-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAu5x2wLr2oH06VQqpkCih8a+g3njxoXu/GZ0pIWWPh2tmbK8B\nIQI0uG3NXt7l46JZEku0UoF1q+N4xMuRL1iSMFPpZhXiuP2igiu9Kh+RGPYXkhJl[...]-----END RSA PRIVATE KEY-----\n\""
+
 }
 variable "ssh_public_key" { 
   description = "Public SSH key string corresponding to the private \"opc_private_key_path\" private key. e.g. \"ssh-rsa AAAAAAAAAA...longlongstring..AAAAAAAAAA\""
